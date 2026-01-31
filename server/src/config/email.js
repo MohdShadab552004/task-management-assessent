3d7691f90889f30a94d25611aee44f3e82284f83
Mohd Shadab <mohdshadab.552004@gmail.com>
2026-01-31 17:40:18 +0530

    fix email issu

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -1,9 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    port: 465,
-    secure: true,
+    host: process.env.EMAIL_HOST || "smtp.gmail.com",
+    port: process.env.EMAIL_PORT || 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -53,16 +53,13 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 // Generate and hash password token
 userSchema.methods.getResetPasswordToken = function () {
-    // Generate token
     const resetToken = crypto.randomBytes(20).toString('hex');
 
-    // Hash token and set to resetPasswordToken field
     this.resetPasswordToken = crypto
         .createHash('sha256')
         .update(resetToken)
         .digest('hex');
 
-    // Set expire
     this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
     return resetToken;
